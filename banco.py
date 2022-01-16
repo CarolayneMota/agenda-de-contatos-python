277a3c946dddc975d268af4f1b65a0f1d77a878e
Carolayne Mota <carolaynepf.mota@gmail.com>
2022-01-16 15:41:57 -0300

    funções criadas

---
+++ banco.py
@@ -0,0 +1,36 @@
+def inserir():
+    # cpf, nome, sobrenome, email, telefone, curso,
+    # data_nasc, observacao
+    return None
+
+
+def atualizar_contato():
+    return None
+
+
+def buscar_contato_por_cpf(cpf):
+    return None
+
+
+def buscar_contato_por_email(email):
+    return None
+
+
+def buscar_contato_por_nome(nome):
+    return None
+
+
+def buscar_contato_por_curso(curso):
+    return None
+
+
+def quantidade_de_contatos():
+    return None
+
+
+def deletar_contato_por_cpf(cpf):
+    return None
+
+
+def deletar_contato_por_email(email):
+    return None
